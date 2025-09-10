a78221503f7372a43895776281e51011a701516a
Julio Cesar de Jesus Bonfim <159816707+Juliocesarsenai@users.noreply.github.com>
2025-09-10 11:20:39 -0300

    Add files via upload

---
+++ formulario/js/moeda.js
@@ -0,0 +1,13 @@
+//DOM
+
+const real = document.querySelector('#real')
+const botao = document.querySelector('#botao')
+const resultado = document.querySelector('#resultado')
+
+botao.addEventListener('click',calcular_moeda)
+
+function calcular_moeda(){
+    valor =Number(real.value);
+    dolar = valor / 5.41
+    resultado.textContent = `O valor em dólar é: U$ ${dolar.toFixed(2)}`
+}
